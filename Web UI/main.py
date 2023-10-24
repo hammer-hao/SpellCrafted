116d0174da4a679f9ad77aed6dd270d4ebba78dd
Schwarzii <J.Yin-5@student.tudelft.nl>
2023-10-24 22:29:42 +0200

    Flip the card when getting a valid response.

--- Web UI/main.py
+++ Web UI/main.py
@@ -2,16 +2,19 @@ from browser import ajax, document, bind
 from browser.html import *
 import re
 
+card_w = 969
+card_h = 1352
+card_display_h = 50
 
-def card_container(img_path, con_id, card_w=969, card_h=1352, card_display_h=50):
+
+def card_container(img_path, con_id):
     card_con = DIV(id=con_id, Class='card_container',
-                   style={'width': f'calc({card_display_h}vh * {card_w} / {card_h})',
-                          'height': f'{card_display_h}vh',
-                          'background-image': f"url({img_path})"})
+                   style={'background-image': f"url({img_path})"})
     return card_con
 
 
-card_pos = DIV(Class='card_pos')
+card_pos = DIV(Class='card_pos', style={'width': f'calc({card_display_h}vh * {card_w} / {card_h})',
+                                        'height': f'{card_display_h}vh'})
 
 card_gen = card_container('img/card_template.png', 'card_gen')
 card_title = DIV(id='card_title')
@@ -22,8 +25,10 @@ card_pos <= card_gen
 card_back = card_container('img/card_back.png', 'card_back')
 card_pos <= card_back
 document <= card_pos
-document['card_gen'].style.display = 'none'
+
+# Initialize special style
 document['card_back'].classList.add('pointer')
+document['card_gen'].classList.add('flip_card')
 
 prompt_con = DIV(Class='prompt_container')
 prompt_con <= INPUT(id='prompt', Class='prompt_input')
@@ -47,51 +52,50 @@ def generate(ev):
 
 
 def show_generated_card(resp):
-    print(resp.json)
     resp = resp.json
+    if resp is None:
+        generating.text = '寄'
+    else:
+        card_pos.classList.add('flip_card')
+        print(resp.json)
 
-    card_back.style.display = 'none'
-    card_pos <= card_gen
-    card_gen.style.display = 'initial'
-
-    document['card_name'].text = resp['name']
-    mana = resp['mana'].replace('{', '').replace('}', '')
-    for m in mana:
-        document['card_mana'] <= IMG(src=f'card_icon/mana-{m}.png', Class='icon')
-    document['card_gen'].style.backgroundImage = f"url(card/{mana[-1]}.jpg)"
-    document['card_cost'].text = resp['cost']
-    # print(len(resp['cost']), document['card_cost'].width / 16)
-    document['card_cost'].style.fontSize = f"{document['card_cost'].height / document['card_cost'].scrollHeight * 100}%"
-
-    des = re.sub('\{.\}', '#', resp['description']).split('#')
-    des_icon = re.findall('\{.\}', resp['description'])
-    des_con = document['card_des']
-    print(des)
-    print(des_icon)
-    print()
-    # des_con <= SPAN("Destroy target creature with power 4 or greater. Its controller may return all nonland cards from your graveyard to the battlefield. 4: Look at the top X cards of your library equal to the number of creature cards revealed this turn. You may put that many + 1 /+ 1 counters on it instead. If that card has no mana value 4 or greater than put into your graveyard from the top of your library instead Horde ' s graveyard: Choose a card name. You may look at the top card of your library. Draw 2 cards, then put it into your hand. If it shares a spell from your hand, put the rest into your hand and the rest on the bottom of your library.) Whenever you draw up to one target creature deals damage to a player, you draw two cards.")
-    for s in range(len(des)):
-        if s >= 1:
-            des_con <= IMG(src=f'card_icon/mana-{des_icon[s - 1][1].lower()}.png', Class='icon')
-        des_con <= SPAN(des[s])
-
-    # des_con.style.fontSize = f"{des_con.height /
-    # (-0.002285 * des_con.scrollHeight ** 2 + 1.4979 * des_con.scrollHeight - 26.6) * 16}px "
-    des_con.style.fontSize = f"{des_con.height / des_con.scrollHeight * 100}%"
-    generating.text = 'Reset'
-    generating.classList.add('reset')
-
-    @bind(generating, 'click')
-    def reset_card(ev):
-        card_gen.style.display = 'none'
-        card_back.style.display = 'initial'
         document['card_des'].innerHTML = ''
         document['card_mana'].innerHTML = ''
 
-        ev.target.remove()
-        ev.target.unbind('click', reset_card)
+        document['card_name'].text = resp['name']
+        mana = resp['mana'].replace('{', '').replace('}', '')
+        for m in mana:
+            document['card_mana'] <= IMG(src=f'card_icon/mana-{m}.png', Class='icon')
+        document['card_gen'].style.backgroundImage = f"url(card/{mana[-1]}.jpg)"
+        document['card_cost'].text = resp['cost']
+        # print(len(resp['cost']), document['card_cost'].width / 16)
+        document['card_cost'].style.fontSize = f"{document['card_cost'].height / document['card_cost'].scrollHeight * 100}%"
+
+        des = re.sub('\{.\}', '#', resp['description']).split('#')
+        des_icon = re.findall('\{.\}', resp['description'])
+        des_con = document['card_des']
+        print(des)
+        print(des_icon)
+        print()
+        # des_con <= SPAN("Destroy target creature with power 4 or greater. Its controller may return all nonland cards from your graveyard to the battlefield. 4: Look at the top X cards of your library equal to the number of creature cards revealed this turn. You may put that many + 1 /+ 1 counters on it instead. If that card has no mana value 4 or greater than put into your graveyard from the top of your library instead Horde ' s graveyard: Choose a card name. You may look at the top card of your library. Draw 2 cards, then put it into your hand. If it shares a spell from your hand, put the rest into your hand and the rest on the bottom of your library.) Whenever you draw up to one target creature deals damage to a player, you draw two cards.")
+        for s in range(len(des)):
+            if s >= 1:
+                des_con <= IMG(src=f'card_icon/mana-{des_icon[s - 1][1].lower()}.png', Class='icon')
+            des_con <= SPAN(des[s])
+
+        # des_con.style.fontSize = f"{des_con.height /
+        # (-0.002285 * des_con.scrollHeight ** 2 + 1.4979 * des_con.scrollHeight - 26.6) * 16}px "
+        des_con.style.fontSize = f"{des_con.height / des_con.scrollHeight * 100}%"
+        generating.text = 'Reset'
+        generating.classList.add('reset')
+
+        @bind(generating, 'click')
+        def reset_card(ev):
+            ev.target.remove()
+            ev.target.unbind('click', reset_card)
+            card_pos.classList.remove('flip_card')
 
 
-@bind(document['prompt'], 'change')
+@bind(document['prompt'], 'keydown')
 def capitalize_input(ev):
     ev.target.value = ev.target.value.capitalize()
