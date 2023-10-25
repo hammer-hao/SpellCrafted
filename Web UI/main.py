95fd5bfec22fa3c28083f9a132ce7b0efe7c583d
Schwarzii <J.Yin-5@student.tudelft.nl>
2023-10-25 20:10:29 +0200

    Add favicon, display no-mana-color card template, randomly display card back

--- Web UI/main.py
+++ Web UI/main.py
@@ -1,6 +1,7 @@
 from browser import ajax, document, bind
 from browser.html import *
 import re
+import random
 
 card_w = 969
 card_h = 1352
@@ -22,7 +23,9 @@ card_title <= DIV(id='card_name') + DIV(id='card_mana')
 card_gen <= card_title + DIV(id='card_cost') + DIV(id='card_des')
 card_pos <= card_gen
 
-card_back = card_container('img/card_back.png', 'card_back')
+back_img = ['card_back.png', 'card_back_sun.jpg', 'card_back_mtg.jpg']
+back_img_choice = random.randint(0, 2)
+card_back = card_container(f'img/{back_img[back_img_choice]}', 'card_back')
 card_pos <= card_back
 document <= card_pos
 
@@ -49,6 +52,8 @@ def generate(ev):
     document <= generating
     generating.text = 'Generating...'
     generating.classList.remove('reset')
+    document['card_des'].innerHTML = ''
+    document['card_mana'].innerHTML = ''
 
 
 def show_generated_card(resp):
@@ -59,14 +64,14 @@ def show_generated_card(resp):
         card_pos.classList.add('flip_card')
         print(resp)
 
-        document['card_des'].innerHTML = ''
-        document['card_mana'].innerHTML = ''
-
         document['card_name'].text = resp['name']
         mana = resp['mana'].replace('{', '').replace('}', '').lower()
         for m in mana:
-            document['card_mana'] <= IMG(src=f'card_icon/mana-{m}.png', Class='icon')
-        document['card_gen'].style.backgroundImage = f"url(card/{mana[-1]}.jpg)"
+            document['card_mana'] <= IMG(src=f'mana_icon/mana-{m}.png', Class='icon')
+        if mana[-1].isalpha():
+            document['card_gen'].style.backgroundImage = f"url(card/{mana[-1]}.jpg)"
+        else:
+            document['card_gen'].style.backgroundImage = f"url(img/card_template.png)"
         document['card_cost'].text = resp['cost']
         # print(len(resp['cost']), document['card_cost'].width / 16)
         document['card_cost'].style.fontSize = f"{document['card_cost'].height / document['card_cost'].scrollHeight * 100}%"
@@ -80,7 +85,7 @@ def show_generated_card(resp):
         # des_con <= SPAN("Destroy target creature with power 4 or greater. Its controller may return all nonland cards from your graveyard to the battlefield. 4: Look at the top X cards of your library equal to the number of creature cards revealed this turn. You may put that many + 1 /+ 1 counters on it instead. If that card has no mana value 4 or greater than put into your graveyard from the top of your library instead Horde ' s graveyard: Choose a card name. You may look at the top card of your library. Draw 2 cards, then put it into your hand. If it shares a spell from your hand, put the rest into your hand and the rest on the bottom of your library.) Whenever you draw up to one target creature deals damage to a player, you draw two cards.")
         for s in range(len(des)):
             if s >= 1:
-                des_con <= IMG(src=f'card_icon/mana-{des_icon[s - 1][1].lower()}.png', Class='icon')
+                des_con <= IMG(src=f'mana_icon/mana-{des_icon[s - 1][1].lower()}.png', Class='icon')
             des_con <= SPAN(des[s])
 
         # des_con.style.fontSize = f"{des_con.height /
